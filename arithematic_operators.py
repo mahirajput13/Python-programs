d22d2bb6ab17469af018390839fc6bff83d1dec3
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 22:47:37 +0530

    arithmeticOperators

---
+++ arithematic_operators.py
@@ -0,0 +1,20 @@
+#program performing arithematic operations
+#asking user for input
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+#performing calculations
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+floordiv = a // b
+
+#printing result to the user
+print("addition is : ", add)
+print("subtraction is : ", sub)
+print("multiplication is : ", mul)
+print("division is : ", div)
+print("modulus is : ", mod)
+print("floor division is : ", floordiv)
