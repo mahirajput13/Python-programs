2021f623adfcbfc392f257a23b375e242c19d0b1
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 22:54:26 +0530

    swappingNumbersProgram

---
+++ swapping_numbers.py
@@ -0,0 +1,18 @@
+#input two num by the user
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+#printing number before swapping
+print("Before swapping.")
+print("First number : ", a)
+print("Second number : ", b)
+
+#calculation of swapping numbers
+c = a
+a = b
+b = c
+
+#printing numbers after swapping
+print("After swapping.")
+print("First number : ", a)
+print("Second number : ", b)
