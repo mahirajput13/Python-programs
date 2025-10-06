0d66ff61c3c44fa9da9438b91af501d05f68c83f
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 22:48:48 +0530

    calculatesLengthOfString

---
+++ calc_length_string.py
@@ -0,0 +1,7 @@
+# ask user to enter his/her name and store it in a variable
+username = input("What is your username?")
+
+#len function calculate the length of the name
+length = len(username)
+
+print(length)
