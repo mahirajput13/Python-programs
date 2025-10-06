85d2c0587634a79e62d7cd1f4e94110738c828fa
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 22:48:13 +0530

    bandNameGeneratorProgram

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+#writing a welcome msg to the user
+print("Hey! this is a band name generator project")
+
+#aksing user to enter some details
+city = input("In which city you grew up in?\n")
+pet = input("Enter your pet name\n")
+
+#printing band name of the user
+print("Congrats! this is your band name : " + " " + city + " " + pet)
