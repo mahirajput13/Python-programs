67ba00754bbec13aa0ff1aa66d4c147ff0b029b1
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 22:50:11 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+#subscripting
+print("Hello"[2])
+
+#strings
+print("123" + "345")
+
+#integers
+print(123 + 345)
+
+#large integers
+print(123_456_789)
+print(123456789)
+print(123,456,789)
+
+#float
+print(3.1457)
+
+#boolean
+print(True)
+print(False)
